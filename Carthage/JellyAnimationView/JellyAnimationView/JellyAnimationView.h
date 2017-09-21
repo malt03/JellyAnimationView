2a7ebfbad974a73db14883e8ac7dd17c85121e68
Koji Murata <koji.murata@dena.com>
2017-09-21 14:21:44 +0900

    carthage

---
+++ Carthage/JellyAnimationView/JellyAnimationView/JellyAnimationView.h
@@ -0,0 +1,19 @@
+//
+//  JellyAnimationView.h
+//  JellyAnimationView
+//
+//  Created by Koji Murata on 2017/09/21.
+//  Copyright © 2017年 Koji Murata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JellyAnimationView.
+FOUNDATION_EXPORT double JellyAnimationViewVersionNumber;
+
+//! Project version string for JellyAnimationView.
+FOUNDATION_EXPORT const unsigned char JellyAnimationViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JellyAnimationView/PublicHeader.h>
+
+
